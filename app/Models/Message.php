0ca3b7cae93c00810d5cde5f0fb2faa157e502fc
MrKacmaz <alperen703.akm@gmail.com>
2024-09-04 12:24:48 +0300

    Update attributes and added relations to message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['room_id', 'user_id', 'message'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
