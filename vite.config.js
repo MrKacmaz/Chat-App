8ee83dae8ac3f5e2ada03bac8c6e9ee2c544c402
MrKacmaz <alperen703.akm@gmail.com>
2024-09-04 12:47:41 +0300

    update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import {resolve} from 'path';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'resources/js/app.js'),
+            },
+        },
+        minify: true,
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '@': resolve(__dirname, 'resources/js'),
+        },
+    },
 });
